f6a43232363aef1345e34eb7d7743e871a2756ee
saweenthapa <57078175+saweenthapa@users.noreply.github.com>
2024-03-20 00:14:28 -0500

    merge sort implementation

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import (
 	"fmt"
 
+	srt "github.com/dev-crusader404/leetcode-solutions/datastructure/sorting"
 	lc "github.com/dev-crusader404/leetcode-solutions/leetcode"
 )
 
 func main() {
-	fmt.Println(lc.ContainsNearbyDuplicate([]int{1, 2, 3, 1, 2, 3}, 2))
+	// fmt.Println(lc.ContainsNearbyDuplicate([]int{1, 2, 3, 1, 2, 3}, 2))
 
-	fmt.Println(lc.AddDigits(54613475))
-	fmt.Println(lc.AddDigits2(54613475))
+	// fmt.Println(lc.AddDigits(54613475))
+	// fmt.Println(lc.AddDigits2(54613475))
 	fmt.Println(lc.IsUgly2(30))
+	srt.SortWithMerge()
 }
