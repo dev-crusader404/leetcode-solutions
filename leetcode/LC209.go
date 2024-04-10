c0d620e74c87386bf41ffa95314ee7c706048cbb
saweenthapa <57078175+saweenthapa@users.noreply.github.com>
2024-04-09 19:57:25 -0500

    LC209 added

---
+++ leetcode/LC209.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "math"
+
+func minSubArrayLen(target int, nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	minLength := math.MaxInt32
+	leftIndex := 0
+	sum := 0
+	for i, v := range nums {
+		sum += v
+		for sum >= target {
+			if minLength > (i - leftIndex + 1) {
+				minLength = i - leftIndex + 1
+			}
+			sum -= nums[leftIndex]
+			leftIndex++
+		}
+	}
+
+	if minLength == math.MaxInt32 {
+		minLength = 0
+	}
+	return minLength
+}
