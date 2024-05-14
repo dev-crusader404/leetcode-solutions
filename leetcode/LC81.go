e4397333abf2249176d5680b35ba93be172564be
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-05-14 16:47:33 -0500

    lc81 solution added

--- leetcode/LC81.go
+++ leetcode/LC81.go
@@ -34,3 +34,34 @@ nums is guaranteed to be rotated at some pivot.
 -104 <= target <= 104
 
 */
+
+func SearchII(nums []int, target int) bool {
+	left, right := 0, len(nums)-1
+
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return true
+		}
+		// left part is sorted
+		if nums[left] < nums[mid] {
+			// target is in rotated section
+			if nums[mid] < target || nums[left] > target {
+				left = mid + 1
+			} else {
+				right = mid - 1
+			}
+		} else if nums[left] > nums[mid] {
+			// right part is sorted
+			if nums[mid] > target || nums[right] < target {
+				right = mid - 1
+			} else {
+				left = mid + 1
+			}
+
+		} else {
+			left++
+		}
+	}
+	return false
+}
