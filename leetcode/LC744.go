4ed5bf7570a2b4f47d50fbe3f31a3debd7b61547
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-05-15 13:51:52 -0500

    efficient solution

--- leetcode/LC744.go
+++ leetcode/LC744.go
@@ -2,6 +2,7 @@ package leetcode
 
 import "fmt"
 
+// Time complexity - O(N)
 func nextGreatestLetter(letters []byte, target byte) byte {
 	last := letters[len(letters)-1]
 	if target == 'z' || last < target {
@@ -16,7 +17,28 @@ func nextGreatestLetter(letters []byte, target byte) byte {
 	return letters[0]
 }
 
+// Time complexity - O(logN)
+func nextGreater2(letters []byte, target byte) byte {
+	last := letters[len(letters)-1]
+	if target == 'z' || last < target {
+		return letters[0]
+	}
+
+	low, high := 0, len(letters)-1
+
+	for low < high {
+		mid := (low + high) / 2
+		if letters[mid] <= target {
+			low = mid + 1
+		} else {
+			high = mid
+		}
+	}
+	return letters[low]
+}
+
 func NextGreaterLetter() {
 	list := []byte{'c', 'f', 'j'}
 	fmt.Println(string(nextGreatestLetter(list, 'c')))
+	fmt.Println(string(nextGreater2(list, 'c')))
 }
