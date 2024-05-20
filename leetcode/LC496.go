5c6f19866eac10ec74c3d2a16a80664de70094ef
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-05-20 16:08:12 -0500

    alternative solution

--- leetcode/LC496.go
+++ leetcode/LC496.go
@@ -3,6 +3,28 @@ package leetcode
 import "fmt"
 
 func nextGreaterElement(nums1 []int, nums2 []int) []int {
+	stack := make([]int, 0)
+	gMap := make(map[int]int)
+
+	for _, v := range nums2 {
+		for len(stack) > 0 && v > stack[len(stack)-1] {
+			gMap[stack[len(stack)-1]] = v
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, v)
+	}
+
+	for i, v := range nums1 {
+		if x, ok := gMap[v]; ok {
+			nums1[i] = x
+		} else {
+			nums1[i] = -1
+		}
+	}
+	return nums1
+}
+
+func nextGreaterElement2(nums1 []int, nums2 []int) []int {
 	m1 := make(map[int]int)
 	for i, v := range nums2 {
 		m1[v] = i
@@ -30,4 +52,5 @@ func RunLC496() {
 	a := []int{4, 1, 2}
 	b := []int{1, 3, 4, 2}
 	fmt.Println(nextGreaterElement(a, b))
+	fmt.Println(nextGreaterElement2(a, b))
 }
