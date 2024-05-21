2b0fce903ba909fecfff8c0779765c6998105cd9
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-05-21 13:02:40 -0500

    alternate solution

--- leetcode/LC503.go
+++ leetcode/LC503.go
@@ -3,6 +3,23 @@ package leetcode
 import "fmt"
 
 func nextGreaterElements(nums []int) []int {
+	n := len(nums)
+	result := make([]int, n)
+	for i, _ := range result {
+		result[i] = -1
+	}
+	stack := make([]int, 0)
+	for i := 0; i < 2*len(nums); i++ {
+		for len(stack) > 0 && nums[i%n] > nums[stack[len(stack)-1]] {
+			result[stack[len(stack)-1]] = nums[i%n]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, i%n)
+	}
+	return result
+}
+
+func nextGreaterElements2(nums []int) []int {
 	result := make([]int, 0)
 
 	for i := 0; i < len(nums); i++ {
@@ -25,4 +42,5 @@ func nextGreaterElements(nums []int) []int {
 func RunLC503() {
 	a := []int{1, 5, 4, 3, 2, 1}
 	fmt.Println(nextGreaterElements(a))
+	fmt.Println(nextGreaterElements2(a))
 }
