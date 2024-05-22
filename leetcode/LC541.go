eaa62f2149718dabc74e6e1ac8476e05000c3e72
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-05-22 16:54:57 -0500

    bug fix

--- leetcode/LC541.go
+++ leetcode/LC541.go
@@ -4,18 +4,10 @@ import "fmt"
 
 func reverseStr(s string, k int) string {
 	var left, right int
-	var all bool
 	b := []byte(s)
-	if k > len(s) {
-		right = len(s) - 1
-		all = true
-	}
 
 	for i := 0; i < len(s); i = i + 2*k {
-		left = i
-		if !all {
-			right = i + k - 1
-		}
+		left, right = i, min(i+k-1, len(s)-1)
 
 		for left < right && right < len(s) {
 			b[left], b[right] = b[right], b[left]
@@ -28,6 +20,6 @@ func reverseStr(s string, k int) string {
 }
 
 func RunLC541() {
-	s := "abcd"
-	fmt.Println(reverseStr(s, 5))
+	s := "hyzqyljrnigxvdtneasepfahmtyhlohwxmkqcdfehybknvdmfrfvtbsovjbdhevlfxpdaovjgunjqlimjkfnqcqnajmebeddqsgl"
+	fmt.Println(reverseStr(s, 39))
 }
