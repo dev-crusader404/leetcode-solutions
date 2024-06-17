0cdbb09e4d701dff0acd0bfc826760d1256b1b3f
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-06-17 14:49:44 -0500

    optimal solution

--- leetcode/LC42.go
+++ leetcode/LC42.go
@@ -2,7 +2,7 @@ package leetcode
 
 import "fmt"
 
-func trap(height []int) int {
+func trap2(height []int) int {
 	if len(height) < 3 {
 		return 0
 	}
@@ -23,7 +23,34 @@ func trap(height []int) int {
 	return total
 }
 
+func trap(height []int) int {
+	if len(height) < 3 {
+		return 0
+	}
+	var maxL, maxR, total int
+	left, right := 0, len(height)-1
+
+	for left < right {
+		if height[left] >= height[right] {
+			if maxR < height[right] {
+				maxR = height[right]
+			} else {
+				total += maxR - height[right]
+			}
+			right--
+		} else {
+			if maxL < height[left] {
+				maxL = height[left]
+			} else {
+				total += maxL - height[left]
+			}
+			left++
+		}
+	}
+	return total
+}
+
 func RunLC42() {
-	h := []int{4, 2, 0, 3, 2, 5}
+	h := []int{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}
 	fmt.Println(trap(h))
 }
