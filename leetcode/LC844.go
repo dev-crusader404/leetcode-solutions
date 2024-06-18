fee57e25e90902594442baddcb7d54b3e3c16b5c
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-06-18 14:51:20 -0500

    optimal solution

--- leetcode/LC844.go
+++ leetcode/LC844.go
@@ -5,6 +5,37 @@ import (
 	"strings"
 )
 
+func backspaceCompares(s string, t string) bool {
+	m := len(s) - 1
+	n := len(t) - 1
+	for {
+		if m >= 0 && s[m] == '#' {
+			moveBack(s, &m)
+		}
+		if n >= 0 && t[n] == '#' {
+			moveBack(t, &n)
+		}
+		if m >= 0 && n >= 0 && s[m] == t[n] {
+			m--
+			n--
+		} else {
+			break
+		}
+	}
+	return (m == -1 && n == -1)
+}
+
+func moveBack(w string, pos *int) {
+	delete := 2
+	for delete > 0 && (*pos) >= 0 {
+		(*pos)--
+		delete--
+		if (*pos) >= 0 && w[(*pos)] == '#' {
+			delete += 2
+		}
+	}
+}
+
 func backspaceCompare(s string, t string) bool {
 	a := process(s)
 	b := process(t)
@@ -26,4 +57,5 @@ func process(s string) string {
 
 func RunLC844() {
 	fmt.Println(backspaceCompare("a#c", "b"))
+	fmt.Println(backspaceCompares("nzp#o#g", "b#nzp#o#g"))
 }
