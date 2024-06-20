6061801d80778bb9a37b6e8444ae53cdbce14ffa
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-06-20 15:26:11 -0500

    iterative solution

--- leetcode/LC430.go
+++ leetcode/LC430.go
@@ -7,23 +7,47 @@ type DLNode struct {
 	Child *DLNode
 }
 
+func flattenIterative(root *DLNode) *DLNode {
+	if root == nil {
+		return root
+	}
+	curr := root
+	for curr != nil {
+		if curr.Child != nil {
+			tail := curr.Child
+			for tail.Next != nil {
+				tail = tail.Next
+			}
+			tail.Next = curr.Next
+			if curr.Next != nil {
+				curr.Next.Prev = tail
+			}
+			curr.Next = curr.Child
+			curr.Child.Prev = curr
+			curr.Child = nil
+		}
+		curr = curr.Next
+	}
+	return root
+}
+
 func flatten(root *DLNode) *DLNode {
 	if root == nil {
 		return root
 	}
 	curr := root
 
-	flattenNode(curr, nil)
+	flattenNode(curr)
 	return root
 }
 
-func flattenNode(curr, parent *DLNode) *DLNode {
+func flattenNode(curr *DLNode) *DLNode {
 	var end *DLNode
 	for curr.Next != nil && curr.Child == nil {
 		curr = curr.Next
 	}
 	if curr.Child != nil {
-		end = flattenNode(curr.Child, curr)
+		end = flattenNode(curr.Child)
 		end.Next = curr.Next
 		curr.Child.Prev = curr
 	}
@@ -34,9 +58,6 @@ func flattenNode(curr, parent *DLNode) *DLNode {
 		curr.Next = curr.Child
 	}
 	curr.Child = nil
-	if parent == nil {
-		return curr
-	}
 	for curr.Next != nil {
 		curr = curr.Next
 	}
