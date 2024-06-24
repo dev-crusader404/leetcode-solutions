254f8f7636835425eb20aa1134e6b65f0040dc7f
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-06-24 14:13:31 -0500

    logn solution only

--- leetcode/LC34.go
+++ leetcode/LC34.go
@@ -40,7 +40,52 @@ func findboundary(mid int, nums []int) []int {
 	return []int{i, j}
 }
 
+func searchRange2(nums []int, target int) []int {
+	defValue := []int{-1, -1}
+
+	if len(nums) == 0 {
+		return defValue
+	}
+
+	low, high := 0, len(nums)-1
+	index := binarySearch(low, high, target, nums)
+	if index == -1 {
+		return defValue
+	}
+	rightIndex, leftIndex := index, index
+	var temp int
+	for leftIndex != -1 {
+		temp = leftIndex
+		leftIndex = binarySearch(low, leftIndex-1, target, nums)
+	}
+	leftIndex = temp
+
+	for rightIndex != -1 {
+		temp = rightIndex
+		rightIndex = binarySearch(rightIndex+1, high, target, nums)
+	}
+	rightIndex = temp
+
+	return []int{leftIndex, rightIndex}
+}
+
+func binarySearch(low, high, target int, nums []int) int {
+	for low <= high {
+		mid := (low + high) / 2
+
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] < target {
+			low = mid + 1
+		} else {
+			high = mid - 1
+		}
+	}
+	return -1
+}
+
 func RunLC34() {
 	arr := []int{5, 7, 7, 8, 8, 10}
-	fmt.Println(searchRange(arr, 5))
+	fmt.Println(searchRange2(arr, 5))
 }
