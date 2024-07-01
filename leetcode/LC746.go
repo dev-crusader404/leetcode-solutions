4dc3a2580b331cc0e11e47cc07f29eb483d6f2be
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-07-01 18:11:56 -0500

    optimized solution

--- leetcode/LC746.go
+++ leetcode/LC746.go
@@ -1,5 +1,6 @@
 package leetcode
 
+// T: O(N) 		S: O(N)
 func minCostClimbingStairs(cost []int) int {
 	if len(cost) < 2 {
 		return -1
@@ -13,4 +14,20 @@ func minCostClimbingStairs(cost []int) int {
 	}
 
 	return min(result[0], result[1])
+}
+
+// T: O(N)		S: O(1)
+func minCostClimbingStairs2(cost []int) int {
+	if len(cost) < 2 {
+		return -1
+	}
+	topOne := cost[0]
+	topTwo := cost[1]
+	for i := 2; i < len(cost); i++ {
+		curr := cost[i] + min(topOne, topTwo)
+		topOne = topTwo
+		topTwo = curr
+	}
+
+	return min(topOne, topTwo)
 }
