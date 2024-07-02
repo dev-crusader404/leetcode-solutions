d829c77298c2a7ddf1d8a99aae19a1ae69b88b56
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-07-02 16:27:11 -0500

    closure solution

--- leetcode/LC688.go
+++ leetcode/LC688.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "fmt"
+
 var chessMoves = [][]int{{-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}}
 
 func knightProbability(n int, k int, row int, column int) float64 {
@@ -16,4 +18,32 @@ func knightProbability(n int, k int, row int, column int) float64 {
 	}
 
 	return total
+}
+
+func knightProbability2(n int, k int, row int, column int) float64 {
+	var (
+		dp                = make(map[string]float64)
+		moves             = [][]int{{-2, -1}, {-1, -2}, {1, -2}, {2, -1}, {-2, 1}, {-1, 2}, {1, 2}, {2, 1}}
+		probabilitySolver func(k, row, column int) float64
+	)
+
+	probabilitySolver = func(k, row, column int) float64 {
+		if row < 0 || column < 0 || row >= n || column >= n {
+			return 0
+		}
+		if k == 0 {
+			return 1
+		}
+		key := fmt.Sprintf("%d-%d-%d", k, row, column)
+		if val, ok := dp[key]; ok {
+			return val
+		}
+		var total float64
+		for _, v := range moves {
+			total += probabilitySolver(k-1, row+v[0], column+v[1]) * 0.125
+		}
+		dp[key] = total
+		return dp[key]
+	}
+	return probabilitySolver(k, row, column)
 }
