2a3534b7dee9478306ccd3d1c4e92b19f48d0fff
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-07-03 17:33:48 -0500

    topological solution

--- leetcode/LC310.go
+++ leetcode/LC310.go
@@ -5,6 +5,49 @@ import (
 	"math"
 )
 
+// Topological Sort T: O(E+N^2) S: O(N+E)
+func findMinHeightTrees(n int, edges [][]int) []int {
+	if n < 3 {
+		var res []int
+		for i := 0; i < n; i++ {
+			res = append(res, i)
+		}
+		return res
+	}
+
+	list := make([][]int, n)
+	indegree := make([]int, n)
+
+	for _, v := range edges {
+		indegree[v[0]]++
+		indegree[v[1]]++
+		list[v[0]] = append(list[v[0]], v[1])
+		list[v[1]] = append(list[v[1]], v[0])
+	}
+	queue := make([]int, 0)
+	for i, val := range indegree {
+		if val == 1 {
+			queue = append(queue, i)
+		}
+	}
+
+	for n > 2 {
+		size := len(queue)
+		n -= size
+		for i := 0; i < size; i++ {
+			indegree[queue[i]]--
+			for _, k := range list[queue[i]] {
+				indegree[k]--
+				if indegree[k] == 1 {
+					queue = append(queue, k)
+				}
+			}
+		}
+		queue = queue[size:]
+	}
+	return queue
+}
+
 // This is recursive solution works. However, time complexity is greater so, might get time limit exceeded for large number of nodes
 func findMinHeightTrees2(n int, edges [][]int) []int {
 	var findMaxHeight func(height, node int) int
@@ -44,6 +87,8 @@ func findMinHeightTrees2(n int, edges [][]int) []int {
 func RunLC310() {
 	// a := [][]int{{1, 0}, {1, 2}, {1, 3}}
 	a := [][]int{{3, 0}, {3, 1}, {3, 2}, {3, 4}, {5, 4}}
+	p := findMinHeightTrees(6, a)
+	fmt.Println(p)
 	r := findMinHeightTrees2(6, a)
 	fmt.Println(r)
 }
