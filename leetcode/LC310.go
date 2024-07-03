b36bb6bd7c118006535e876b8ce8c4c9327e3bd6
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-07-03 15:58:14 -0500

    recursive solution

--- leetcode/LC310.go
+++ leetcode/LC310.go
@@ -1,14 +1,34 @@
 package leetcode
 
-import "math"
+import (
+	"fmt"
+	"math"
+)
 
-func findMinHeightTrees(n int, edges [][]int) []int {
-	list := findAdjacency(n, edges)
+// This is recursive solution works. However, time complexity is greater so, might get time limit exceeded for large number of nodes
+func findMinHeightTrees2(n int, edges [][]int) []int {
+	var findMaxHeight func(height, node int) int
+	list := make([][]int, n)
+	for _, v := range edges {
+		list[v[0]] = append(list[v[0]], v[1])
+		list[v[1]] = append(list[v[1]], v[0])
+	}
 	minH := math.MaxInt
 	var result []int
 	for i := 0; i < n; i++ {
 		seen := make(map[int]struct{})
-		h := findMinHeight(list, seen, math.MaxInt, i)
+		findMaxHeight = func(height, node int) int {
+			seen[node] = struct{}{}
+			newHeight := height + 1
+			for i := 0; i < len(list[node]); i++ {
+				curr := list[node][i]
+				if _, ok := seen[curr]; !ok {
+					height = max(height, findMaxHeight(newHeight, curr))
+				}
+			}
+			return height
+		}
+		h := findMaxHeight(0, i)
 		if h > minH {
 			continue
 		}
@@ -21,25 +41,9 @@ func findMinHeightTrees(n int, edges [][]int) []int {
 	return result
 }
 
-func findMinHeight(list [][]int, seen map[int]struct{}, height, node int) int {
-	if _, ok := seen[node]; ok {
-		return height
-	}
-
-	seen[node] = struct{}{}
-	newHeight := height + 1
-	for i := 0; i < len(list[node]); i++ {
-		curr := list[node][i]
-		height = min(height, findMinHeight(list, seen, newHeight, curr))
-	}
-	return height
-}
-
-func findAdjacency(n int, edges [][]int) [][]int {
-	l := make([][]int, n)
-	for _, v := range edges {
-		l[v[0]] = append(l[v[0]], v[1])
-		l[v[1]] = append(l[v[1]], v[0])
-	}
-	return l
+func RunLC310() {
+	// a := [][]int{{1, 0}, {1, 2}, {1, 3}}
+	a := [][]int{{3, 0}, {3, 1}, {3, 2}, {3, 4}, {5, 4}}
+	r := findMinHeightTrees2(6, a)
+	fmt.Println(r)
 }
