95914623c66f5d1758dba63b5652782843dcf382
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-07-08 22:58:44 -0500

    ascii sum

--- leetcode/LC389.go
+++ leetcode/LC389.go
@@ -1,5 +1,17 @@
 package leetcode
 
+func findTheDifference2(s string, t string) byte {
+	var s1, s2 int
+	for _, v := range s {
+		s1 += int(v)
+	}
+
+	for _, k := range t {
+		s2 += int(k)
+	}
+	return byte(s2 - s1)
+}
+
 func findTheDifference(s string, t string) byte {
 	m := make(map[rune]int)
 	for _, v := range s {
