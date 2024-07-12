e3457946320672af9f268b2e58964ed4b0c32ee9
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-07-12 16:47:02 -0500

    char count

--- leetcode/LC520.go
+++ leetcode/LC520.go
@@ -5,6 +5,24 @@ import (
 )
 
 func detectCapitalUse(word string) bool {
+	var uCount, lCount int
+	for _, v := range word {
+		if v >= 'A' && v <= 'Z' {
+			uCount++
+		} else if v >= 'a' && v <= 'z' {
+			lCount++
+		} else if uCount > 1 && lCount > 1 {
+			return false
+		}
+	}
+
+	if len(word) == uCount || len(word) == lCount || (uCount == 1 && word[0] >= 'A' && word[0] <= 'Z') {
+		return true
+	}
+	return false
+}
+
+func detectCapitalUse2(word string) bool {
 	caps := strings.ToUpper(word)
 	lower := strings.ToLower(word)
 	FirstCap := strings.ToUpper(string(word[0])) + strings.ToLower(word[1:])
