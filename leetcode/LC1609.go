d0e2e864c43c7027fa2cacebd36bf164f8ccfffe
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-07-22 23:05:32 -0500

    bug fix

--- leetcode/LC1609.go
+++ leetcode/LC1609.go
@@ -34,7 +34,7 @@ func isEvenOddTree(root *TreeNode) bool {
 				result = true
 				if i > 0 {
 					result = isEven == (prev < node.Val)
-					if !result {
+					if prev == node.Val || !result {
 						return false
 					}
 				}
@@ -50,7 +50,7 @@ func isEvenOddTree(root *TreeNode) bool {
 }
 
 func RunLC1609() {
-	l := []any{1, 10, 4, 3, nil, 7, 9, 12, 8, 6, nil, nil, 2}
+	l := []any{13, 34, 32, 23, 25, 27, 29, 44, 40, 36, 34, 30, 30, 28, 26, 3, 7, 9, 11, 15, 17, 21, 25, nil, nil, 27, 31, 35, nil, 37, nil, 30, nil, 26, nil, nil, nil, 24, nil, 20, 16, 12, 10, nil, nil, 8, nil, nil, nil, nil, nil, 6, nil, nil, nil, nil, nil, 15, 19, nil, nil, nil, nil, 23, nil, 27, 29, 33, 37, nil, nil, nil, nil, nil, nil, 48, nil, nil, nil, 46, nil, nil, nil, 42, 38, 34, 32, nil, nil, nil, nil, 19}
 	tree := BuildTree(l)
 	fmt.Println(isEvenOddTree(tree))
 }
