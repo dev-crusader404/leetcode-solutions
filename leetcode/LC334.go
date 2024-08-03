3ede7c9ff8782bfc5c69d9ac014dc15e6bcac08e
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-08-02 21:55:59 -0500

    increasing triplet

--- leetcode/LC334.go
+++ leetcode/LC334.go
@@ -1,11 +1,29 @@
 package leetcode
 
-func reverseString(s []byte) {
-	start, end := 0, len(s)-1
+import (
+	"fmt"
+	"math"
+)
 
-	for start < end {
-		s[start], s[end] = s[end], s[start]
-		start++
-		end--
+func increasingTriplet(nums []int) bool {
+	min1, min2 := math.MaxInt, math.MaxInt
+	if len(nums) < 3 {
+		return false
 	}
+
+	for _, v := range nums {
+		if v <= min1 {
+			min1 = v
+		} else if v <= min2 {
+			min2 = v
+		} else {
+			return true
+		}
+	}
+	return false
+}
+
+func RunLC334() {
+	n := []int{2, 1, 5, 4, 3, 6}
+	fmt.Println(increasingTriplet(n))
 }
