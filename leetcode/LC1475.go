c0ac2c1e42d52652b15d20824b88f582da6f13a9
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-08-05 22:27:18 -0500

    alternative solution

--- leetcode/LC1475.go
+++ leetcode/LC1475.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "fmt"
+
 func finalPrices(prices []int) []int {
 	stk := []int{}
 	res := make([]int, len(prices))
@@ -14,4 +16,21 @@ func finalPrices(prices []int) []int {
 		stk = append(stk, prices[i])
 	}
 	return res
+}
+
+func finalPrices2(prices []int) []int {
+	stk := make([]int, 0, len(prices))
+	for i, v := range prices {
+		for len(stk) > 0 && v <= prices[stk[len(stk)-1]] {
+			prices[stk[len(stk)-1]] -= v
+			stk = stk[:len(stk)-1]
+		}
+		stk = append(stk, i)
+	}
+	return prices
+}
+
+func RunLC1475() {
+	a := []int{8, 4, 6, 2, 3}
+	fmt.Println(finalPrices2(a))
 }
