0512fb600503b5f1904957d1d65c78bd18566a4d
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-08-06 23:55:46 -0500

    remove duplicate char

---
+++ leetcode/LC1047.go
@@ -0,0 +1,13 @@
+package leetcode
+
+func removeDuplicates(s string) string {
+	stk := []rune{}
+	for _, c := range s {
+		if len(stk) > 0 && stk[len(stk)-1] == c {
+			stk = stk[:len(stk)-1]
+		} else {
+			stk = append(stk, c)
+		}
+	}
+	return string(stk)
+}
