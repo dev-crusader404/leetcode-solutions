6692ad1c6ddc88f3f91cfb7baf8190255e4096cd
saweenthapa <57078175+dev-crusader404@users.noreply.github.com>
2024-08-09 23:04:03 -0500

    solved 728

---
+++ leetcode/LC728.go
@@ -0,0 +1,31 @@
+package leetcode
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func selfDividingNumbers(left int, right int) []int {
+	l := []int{}
+	for i := left; i <= right; i++ {
+		if selfDivide(i) {
+			l = append(l, i)
+		}
+	}
+	return l
+}
+
+func selfDivide(x int) bool {
+	s := strconv.Itoa(x)
+	for _, v := range s {
+		num := int(v - '0')
+		if num == 0 || x%num != 0 {
+			return false
+		}
+	}
+	return true
+}
+
+func RunLC728() {
+	fmt.Println(selfDividingNumbers(1, 22))
+}
