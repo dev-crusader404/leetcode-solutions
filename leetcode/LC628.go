51f8ae1c92efc52f8598615a1dfc83f1abef5b0d
dev-crusader404 <57078175+dev-crusader404@users.noreply.github.com>
2024-09-13 23:19:51 -0500

    solved with O(n)

--- leetcode/LC628.go
+++ leetcode/LC628.go
@@ -1,6 +1,9 @@
 package leetcode
 
-import "sort"
+import (
+	"math"
+	"sort"
+)
 
 func maximumProduct2(nums []int) int {
 	sort.Ints(nums)
@@ -8,4 +11,30 @@ func maximumProduct2(nums []int) int {
 	m1 := nums[0] * nums[1] * nums[n-1]
 	m2 := nums[n-1] * nums[n-2] * nums[n-3]
 	return max(m1, m2)
+}
+
+func maximumProduct3(nums []int) int {
+	m1, m2, m3 := math.MinInt, math.MinInt, math.MinInt
+	n1, n2 := math.MaxInt, math.MaxInt
+
+	for i := range nums {
+		if nums[i] > m1 {
+			m3 = m2
+			m2 = m1
+			m1 = nums[i]
+		} else if nums[i] > m2 {
+			m3 = m2
+			m2 = nums[i]
+		} else if nums[i] > m3 {
+			m3 = nums[i]
+		}
+
+		if nums[i] < n1 {
+			n2 = n1
+			n1 = nums[i]
+		} else if nums[i] < n2 {
+			n2 = nums[i]
+		}
+	}
+	return max(n1*n2*m1, m1*m2*m3)
 }
