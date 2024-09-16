bb6ef34e823c4ef13ee5b0a5c7e80e0a2ccd0fde
dev-crusader404 <57078175+dev-crusader404@users.noreply.github.com>
2024-09-16 17:10:50 -0500

    binary search O(log N)

--- leetcode/LC2529.go
+++ leetcode/LC2529.go
@@ -11,3 +11,30 @@ func maximumCount(nums []int) int {
 	}
 	return max(pos, neg)
 }
+
+func maximumCount2(nums []int) int {
+	n := len(nums)
+	low, high := 0, n-1
+	var pos, neg int
+
+	for low <= high {
+		mid := (low + high) / 2
+		if nums[mid] < 0 {
+			low = mid + 1
+			neg = mid + 1
+		} else if nums[mid] >= 0 {
+			high = mid - 1
+		}
+	}
+	low, high = 0, n-1
+	for low <= high {
+		mid := (low + high) / 2
+		if nums[mid] > 0 {
+			high = mid - 1
+			pos = n - mid
+		} else if nums[mid] <= 0 {
+			low = mid + 1
+		}
+	}
+	return max(pos, neg)
+}
