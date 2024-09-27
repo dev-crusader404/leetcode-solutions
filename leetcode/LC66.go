9d9152284d8cf5eae49163ed10bae00ebc1be81f
dev-crusader404 <57078175+dev-crusader404@users.noreply.github.com>
2024-09-26 23:31:45 -0500

    easy solution

--- leetcode/LC66.go
+++ leetcode/LC66.go
@@ -1,6 +1,20 @@
 package leetcode
 
+// Quick solution
 func plusOne(digits []int) []int {
+	for i := len(digits) - 1; i >= 0; i-- {
+		if digits[i] < 9 {
+			digits[i]++
+			return digits
+		}
+		digits[i] = 0
+	}
+	res := make([]int, len(digits)+1)
+	res[0] = 1
+	return res
+}
+
+func plusOne2(digits []int) []int {
 	digits[len(digits)-1] += 1
 	var carry int
 	for i := len(digits) - 1; i >= 0; i-- {
